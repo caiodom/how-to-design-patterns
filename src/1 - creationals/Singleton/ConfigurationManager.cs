e34cd880adde7e545335a387c50e835b10c141ce
caio_dom <caio_henrrique@hotmail.com>
2024-06-18 23:11:33 -0300

    singleton

---
+++ src/1 - creationals/Singleton/ConfigurationManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ConfigurationManager
+    {
+        // Lazy instance ensures thread-safe lazy initialization
+        private static readonly Lazy<ConfigurationManager> lazyInstance =
+            new Lazy<ConfigurationManager>(() => new ConfigurationManager());
+
+        // Dictionary to store configuration settings
+        private readonly Dictionary<string, string> settings;
+
+        // Private constructor to prevent instantiation from outside
+        private ConfigurationManager()
+        {
+            // Initialize the settings dictionary
+            settings = new Dictionary<string, string>();
+
+            // Load default settings (could be from a file, database, etc.)
+            LoadDefaultSettings();
+        }
+
+        // Public static property to get the singleton instance
+        public static ConfigurationManager Instance => lazyInstance.Value;
+
+        // Method to load default settings
+        private void LoadDefaultSettings()
+        {
+            settings["AppTitle"] = "My Application";
+            settings["Version"] = "1.0.0";
+            settings["DefaultLanguage"] = "en";
+        }
+
+        // Method to get a setting value by key
+        public string GetSetting(string key)
+        {
+            if (settings.ContainsKey(key))
+            {
+                return settings[key];
+            }
+            return null;
+        }
+
+        // Method to set a setting value
+        public void SetSetting(string key, string value)
+        {
+            if (settings.ContainsKey(key))
+            {
+                settings[key] = value;
+            }
+            else
+            {
+                settings.Add(key, value);
+            }
+        }
+
+    }
+}
---
+++ src/1 - creationals/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Singleton;
+
+//Access the singleton instance
+ConfigurationManager configManager = ConfigurationManager.Instance;
+
+// Get and display default settings
+Console.WriteLine("AppTitle: " + configManager.GetSetting("AppTitle"));
+Console.WriteLine("Version: " + configManager.GetSetting("Version"));
+Console.WriteLine("DefaultLanguage: " + configManager.GetSetting("DefaultLanguage"));
+
+// Set a new setting
+configManager.SetSetting("AppTitle", "My Updated Application");
+
+// Get and display the updated setting
+Console.WriteLine("Updated AppTitle: " + configManager.GetSetting("AppTitle"));
